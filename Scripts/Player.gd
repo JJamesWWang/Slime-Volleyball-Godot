080dd196ebedda6b7c77e7417c2e6f1e82816ed5
James S. Wang <JJamesWWang@gmail.com>
2020-02-14 04:06:28 -0500

    Move collision handling to _on_Collision

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,77 +11,84 @@ var hovering = false
 var falling = false
 onready var jump_timer = $JumpTimer
 onready var hover_timer = $HoverTimer
-onready var fall_timer = $FallTimer
 
 
 func movement_horizontal():
-    if Input.is_action_pressed('right'):
-        return 1 * hspeed
-    if Input.is_action_pressed('left'):
-        return -1 * hspeed
-    return 0
+	if Input.is_action_pressed('right'):
+		return 1 * hspeed
+	if Input.is_action_pressed('left'):
+		return -1 * hspeed
+	return 0
 
 
 func movement_vertical():
-    if Input.is_action_pressed("up") and not (jumping or hovering or falling):
-        jump_start()
-    if Input.is_action_pressed("down") and jumping:
-        hover_start()
-    
-    if jumping:
-        return -1 * vspeed
-    if falling:
-        return 1 * vspeed
-    return 0    # hovering also returns 0
+	if Input.is_action_pressed("up") and not (jumping or hovering or falling):
+		jump_start()
+	if Input.is_action_pressed("down") and jumping:
+		hover_start()
+	
+	if jumping:
+		return -1 * vspeed
+	if hovering:
+		return 0
+	return 1 * vspeed
 
 
 func jump_start():
-    jumping = true
-    hovering = false
-    falling = false
-    jump_timer.start(jump_time)
+	jumping = true
+	hovering = false
+	falling = false
+	jump_timer.start(jump_time)
 
 
 func hover_start():
-    jump_timer.stop()
-    jumping = false
-    hovering = true
-    falling = false
-    hover_timer.start(hover_time)
+	jump_timer.stop()
+	jumping = false
+	hovering = true
+	falling = false
+	hover_timer.start(hover_time)
 
 
 func fall_start():
-    hover_timer.stop()
-    jumping = false
-    hovering = false
-    falling = true
-    fall_timer.start(jump_time - jump_timer.time_left)
+	jump_timer.stop()
+	hover_timer.stop()
+	jumping = false
+	hovering = false
+	falling = true
 
 
 func land():
-    fall_timer.stop()
-    jumping = false
-    hovering = false
-    falling = false
+	jump_timer.stop()
+	hover_timer.stop()
+	jumping = false
+	hovering = false
+	falling = false
 
 
 func _physics_process(delta):
-    var velocity = Vector2()
-    velocity.x = movement_horizontal()
-    velocity.y = movement_vertical()
+	var velocity = Vector2()
+	velocity.x = movement_horizontal()
+	velocity.y = movement_vertical()
 
-    velocity = move_and_slide(velocity)
-    if velocity.y == 0 and not hovering:
-        land()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		_on_Collision(collision)
+		velocity = move_and_slide(velocity)
+
+
+func _on_Collision(collision):
+	if collision.collider.get_class() == "TileMap":
+		land()
+
 
 
 func _on_JumpTimer_timeout():
-    hover_start()
+	hover_start()
 
 
 func _on_HoverTimer_timeout():
-    fall_start()
+	fall_start()
 
 
 func _on_FallTimer_timeout():
-    land()
+	land()
